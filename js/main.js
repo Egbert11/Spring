f66745e09eecef512bfbe9d026fe9eb5181d2874
gzwufulin <gzwufulin@corp.netease.com>
2014-02-25 21:23:48 +0800

    finishing circle hover event && update style

--- js/main.js
+++ js/main.js
@@ -11,9 +11,17 @@ $(function() {
         $(".panels>[name='" + tabName + "']").addClass("active");
 
         // 显示当前panel上方的箭头
-        var x = $(this).position().left + 83;
+        var x;
+        if ($(this).attr("id") == 'meet')
+            x = 83;
+        else
+            x = $(this).position().left + 83;
         $(".top-arrow").css({'left': x+'px'});
+
+        refreshTip();
 	});
+
+    $('#meet').trigger("click");
 
     // 总榜tab
     var total_rank = $(".total-rank-header ul");
@@ -26,15 +34,53 @@ $(function() {
         var tabName = $(this).attr("id");
         $(".total-rank-panel>[class]").removeClass("active");
         $(".total-rank-panel>[class='" + tabName + "']").addClass("active");
-    });
 
-    var leftPanel = $('.total-rank-panel .active .panel-left ul');
-    leftPanel.find("li a").bind("click", function(){
-       leftPanel.find("li a").removeClass("active");
-        $(this).addClass('active');
+        var leftPanel = $('.total-rank-panel .active .panel-left ul');
+        leftPanel.find("li a").bind("click", function(){
+            leftPanel.find("li a").removeClass("active");
+            $(this).addClass('active');
 
-        // to-do
-        // refresh right panel
+            // to-do
+            // refresh right panel
+        });
     });
 });
 
+function refreshTip() {
+   // 圈圈任务的提示框
+    var active_panel = $("div.panels > div.active");
+    active_panel.find("a.finishing").hover(
+        function(){
+            var whichCircle = $(this).attr('name');
+            switch(whichCircle){
+                case "1":
+                    $(".tip").css({'left': '100px','top': '127px'});
+                    break;
+                case "2":
+                    $(".tip").css({'left': '200px','top': '207px'});
+                    break;
+                case "3":
+                    $(".tip").css({'left': '305px','top': '135px'});
+                    break;
+                case "4":
+                    $(".tip").css({'left': '405px','top': '205px'});
+                    break;
+                case "5":
+                    $(".tip").css({'left': '475px','top': '95px'});
+                    break;
+                case "6":
+                    $(".tip").css({'left': '295px','top': '200px'});
+                    break;
+                case "7":
+                    $(".tip").css({'left': '395px','top': '120px'});
+                    break;
+            }
+            $(".tip").show();
+            return false;
+        },
+        function(){
+            $(".tip").hide();
+            return false;
+        }
+    );
+}
