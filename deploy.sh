e48d78575e99480a24ae8ac98f53e617c219013b
gzwufulin <gzwufulin@corp.netease.com>
2014-02-25 16:11:01 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 cp -r css /home/ychzhou/server/webservice/activeapi/htdocs
 cp -r image /home/ychzhou/server/webservice/activeapi/htdocs
 cp -r js /home/ychzhou/server/webservice/activeapi/htdocs
-cp index.html /home/nijun/project/qiannv/pat/web/index.html
+cp index.html /home/ychzhou/server/webservice/activeapi/htdocs
