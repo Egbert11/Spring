cbd286155dd053166e2601e042ccf39a3c30d2f1
gzwufulin <gzwufulin@corp.netease.com>
2014-02-25 16:07:18 +0800

    add deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r css /home/ychzhou/server/webservice/activeapi/htdocs
+cp -r image /home/ychzhou/server/webservice/activeapi/htdocs
+cp -r js /home/ychzhou/server/webservice/activeapi/htdocs
+cp index.html /home/nijun/project/qiannv/pat/web/index.html
